4b350fc74ee3609683c4ed7274bd5162f48f8069
BigBoa19 <choinoah2005@gmail.com>
2024-10-25 19:26:05 -0400

    Added Database functionality(can permanently store classes)

--- src/components/ClassList.tsx
+++ src/components/ClassList.tsx
@@ -1,22 +1,54 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { db } from '~/lib/api/firebaseConfig';
+import { setDoc, doc, collection, query, onSnapshot } from 'firebase/firestore';
+import { useUser } from '@clerk/nextjs';
+
 
 interface Class {
-  id: string;
   title: string;
   professor: string;
   section: string;
 }
 
 export function ClassList() {
-  const [classes, setClasses] = useState<Class[]>([]);
+  const { user } = useUser();
+  const [classes, setClasses] = useState<any[]>([]);
   const [newClass, setNewClass] = useState({ title: '', professor: '', section: '' });
 
   const addClass = () => {
     if (newClass.title && newClass.professor && newClass.section) {
-      setClasses([...classes, { ...newClass, id: Date.now().toString() }]);
+
       setNewClass({ title: '', professor: '', section: '' });
+      
+      const userId = user?.emailAddresses[0]?.emailAddress;
+      const usersDocRef = doc(db, "Users", userId? userId : "");
+      const classesRef = collection(usersDocRef, "Classes");
+      setDoc(doc(classesRef, newClass.title), {
+        title: newClass.title,
+        professor: newClass.professor,
+        section: newClass.section,
+      });
     }
   };
+
+  useEffect(() => {
+    if (!user) return;
+    const userId = user?.emailAddresses[0]?.emailAddress;
+    const usersDocRef = doc(db, "Users", userId? userId : "");
+    const classesRef = collection(usersDocRef, "Classes");
+    const q = query(classesRef);
+
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const classes = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+      }));
+      setClasses(classes);
+    }, (error) => {
+      console.error('Error getting documents: ', error);
+    });
+
+    return () => unsubscribe();
+  }, [user]);
 
   return (
     <div className="mt-8">
