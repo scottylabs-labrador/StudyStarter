5d4a82bd6210b5300aeb010a05034d5d07e9eaaf
BigBoa19 <choinoah2005@gmail.com>
2024-11-19 21:41:42 -0500

    modified:   src/app/create_account/layout.tsx 	modified:   src/app/create_account/page.tsx

--- src/app/create_account/page.tsx
+++ src/app/create_account/page.tsx
@@ -6,22 +6,6 @@ import { Photo } from "~/types";
 import { useEffect, useState } from "react";
 import { ClassList } from "~/components/ClassList";
 
-function ProfileGrid({ photos }: { photos: Photo[] }) {
-  return (
-    <div className="grid grid-cols-3 gap-4">
-      {photos.map((photo) => (
-        <div key={photo.id} className="w-full aspect-w-1 aspect-h-1 bg-[#DDD]">
-          <img
-            src={photo.url}
-            alt="Uploaded"
-            className="w-full h-full object-cover object-center"
-          />
-        </div>
-      ))}
-    </div>
-  );
-}
-
 function ContinueButton() {
     return (
         <a href="/feed" className="px-4 py-2 border-2 hover:bg-darkAccent hover:border-darkbg text-white font-bold rounded-lg margintop-100 hover:bg-gray-300">
@@ -31,22 +15,7 @@ function ContinueButton() {
 }
 
 export default function ProfilePage() {
-  console.log("why");
-//   if (InClass()) { redirect("/feed");}
   const { user } = useUser();
-  const [photos, setPhotos] = useState<Photo[]>([]);
-
-  useEffect(() => {
-    const loadPhotos = async () => {
-      if (user) {
-        const userPhotos = await getUserPhotos(user.emailAddresses[0]?.emailAddress as string);
-        setPhotos(userPhotos);
-      }
-    };
-
-    loadPhotos();
-  }, [user]);
-
   const displayName = user?.fullName || user?.firstName || user?.username || "User";
 
   return (
@@ -73,9 +42,6 @@ export default function ProfilePage() {
         </div>
       </div>
       <h1 className="text-xl font-bold mb-4 text-white">Add Your Classes Below</h1>
-      <div className="mt-8">
-        <ProfileGrid photos={photos} />
-      </div>
       <ClassList />
       <br></br>
       <ContinueButton />
