670de9c99a46913e1f1f7dde08cea6863064036d
csinger <christophersinger10@gmail.com>
2025-03-21 00:24:47 -0400

    feat: log to bq

---
+++ backend/functions/src/bq/bqService.ts
@@ -0,0 +1,19 @@
+import {BigQuery} from "@google-cloud/bigquery";
+
+const bq = new BigQuery();
+
+const DATASET = "live";
+enum Tables {
+    COURSES = "courses",
+    JOIN = "join_event",
+    GROUPS = "study_groups",
+    USER = "user"
+}
+
+export const logJoinEvent = async (email: string, groupId: string) => {
+  const tstamp = new Date().toISOString();
+  const row = {email: email, group_id: groupId,
+    is_creation_event: false, is_leave_event: false,
+    event_date: tstamp};
+  bq.dataset(DATASET).table(Tables.JOIN).insert(row);
+};
