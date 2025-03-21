6b15d6a9d74a622a354be8fb4dc00605c1f509fa
csinger <christophersinger10@gmail.com>
2025-03-21 18:17:54 -0400

    fix: style

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as admin from "firebase-admin";
 import {onRequest} from "firebase-functions/v2/https";
 import {joinGroup} from "./webapp/joinGroup";
-import { leaveGroup } from "./webapp/leaveGroup";
+import {leaveGroup} from "./webapp/leaveGroup";
 
 admin.initializeApp();
 
