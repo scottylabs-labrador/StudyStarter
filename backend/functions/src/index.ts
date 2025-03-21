65b5ad14286c3a9bce65364d03130cefde389d59
csinger <christophersinger10@gmail.com>
2025-03-21 18:06:58 -0400

    fix: include in index

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,9 +1,11 @@
 import * as admin from "firebase-admin";
 import {onRequest} from "firebase-functions/v2/https";
 import {joinGroup} from "./webapp/joinGroup";
+import { leaveGroup } from "./webapp/leaveGroup";
 
 admin.initializeApp();
 
 const db = admin.firestore();
 
 exports.joinGroup = onRequest((req, res) => joinGroup(db, req, res));
+exports.leaveGroup = onRequest((req, res) => leaveGroup(db, req, res));
